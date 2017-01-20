0013ad256f40d31434ca6a28d22c0f71ea29c141
Ethan Larkham <ethanlarkham@gmail.com>
2017-01-19 20:30:25 -0500

    Basic Transmission Client functionality implemented

--- lib/watcher/transmission_client.ex
+++ lib/watcher/transmission_client.ex
@@ -2,19 +2,106 @@ defmodule Animu.TransmissionClient do
   use GenServer
 
   alias HTTPoison.Response
-  alias Animu.{WatcherCache, Reader}
+  alias Animu.{WatcherCache, Reader, Torrent}
+
+  @url "http://192.168.5.115:9091/transmission/rpc"
 
   def start_link(_,_) do
     GenServer.start_link(__MODULE__, :ok, [])
   end
 
   def init(state) do
-  #    Response{body: body} =
+    HTTPoison.start
+    state = %{session_id: "0", torrents: %{}}
+    {:ok, _, state} = request("session-get", %{}, state)
+    start_timer
     {:ok, state}
   end
 
-  def handle_call(:pop, _from, state) do
+  defp request(method, arguments, state) do
+    options = [recv_timeout: 5000]
 
+    headers = [{"X-Transmission-Session-Id", state.session_id},
+               {"Accept", "application/json; charset=utf-8"}]
+
+    body =
+      %{method: method, arguments: arguments}
+      |> Poison.Encoder.encode([])
+      |> to_string
+
+    case HTTPoison.post(@url, body, headers, options) do
+      {:ok, %Response{status_code: 200, body: body}} ->
+        {:ok, Poison.decode!(body), state}
+
+      {:ok, %Response{status_code: 409, headers: headers}} ->
+        state = %{state | session_id: get_header(headers,"X-Transmission-Session-Id")}
+        request(method, arguments, state)
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+        {:error, %{}, state}
+
+      :else ->
+        {:error, %{}, state}
+    end
+  end
+
+  defp get_header(headers, key) do
+    headers
+    |> Enum.filter(fn({k, _}) -> k == key end)
+    |> hd
+    |> elem(1)
+  end
+
+  def handle_call({:add_torrent, torrent}, _from, state) do
+    method = "torrent-add"
+    arguments =
+      %{"filename"     => torrent.url,
+        "download-dir" => torrent.dir,
+        "paused"       => false,
+      }
+
+    case request(method, arguments, state) do
+      {:ok, %{"result" => "success", "torrent_added" => torrent_status}, state} ->
+        id = torrent_status["id"]
+        torrent = %{torrent | id: id}
+        state = %{state | torrents: Map.put(state.torrents, id, torrent}}
+        {:reply, :ok, state}
+      :else ->
+        {:reply, :error, state}
+    end
+  end
+
+  def handle_info(:check_status, state) do
+    method = "torrent-get"
+    arguments =
+      %{"fields" => ["id", "isFinished", "percentDone"]
+        "ids" => Map.keys(state.torrents)
+      }
+
+    {:ok, response, state} = request(method, arguments, state)
+    torrents =
+      response.arguments.torrents
+      |> Map.new(&({&1["id"], %{progress: &1["percentDone"], finished: &1["isFinished"]}}))
+      |> Map.merge(state.torrents, fn(_k, v1, v2) -> Map.merge(Map.from_struct(v2, v1)) end)
+      |> Enum.map(process)
+      |> Enum.reject(fn({_k, v}) -> v.finished end)
+      |> Map.new(fn({k, v}) -> {k, struct(Torrent, v)} end)
+
+    state = %{state | torrents: torrents}
+
+    start_timer
+    {:noreply, state}
+  end
+
+  defp process({_, torrent}) do
+    if torrent.finished do
+      GenServer.cast(:animu_watcher_cache, {:process, torrent})
+    end
+  end
+
+  defp start_timer do
+    Process.send_after(self, :check_status, (2 * 1000))
   end
 
 end
