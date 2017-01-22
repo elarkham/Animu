882cf6f791263d46291127b6aef6f878b475d4c7
Ethan Larkham <ethanlarkham@gmail.com>
2017-01-22 14:16:22 -0500

    Minor controller refactoring, added episode creation

--- web/controllers/episode_controller.ex
+++ web/controllers/episode_controller.ex
@@ -9,21 +9,21 @@ defmodule Animu.EpisodeController do
     render(conn, "index.json", episodes: episodes)
   end
 
-  #def create(conn, %{"episode" => episode_params}) do
-  #  changeset = Episode.changeset(%Episode{}, series, episode_params)
-  #
-  #  case Repo.insert(changeset) do
-  #    {:ok, episode} ->
-  #      conn
-  #      |> put_status(:created)
-  #      |> put_resp_header("location", episode_path(conn, :show, episode))
-  #      |> render("show.json", episode: episode)
-  #    {:error, changeset} ->
-  #      conn
-  #      |> put_status(:unprocessable_entity)
-  #      |> render(Animu.ChangesetView, "error.json", changeset: changeset)
-  #  end
-  #end
+  def create(conn, %{"episode" => episode_params}) do
+    changeset = Episode.changeset(%Episode{}, episode_params)
+
+    case Repo.insert(changeset) do
+      {:ok, episode} ->
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", episode_path(conn, :show, episode))
+        |> render("show.json", episode: episode)
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(Animu.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
 
   def show(conn, %{"id" => id}) do
     episode = Repo.get!(Episode, id)
@@ -31,7 +31,7 @@ defmodule Animu.EpisodeController do
   end
 
   def update(conn, %{"id" => id, "episode" => episode_params}) do
-    episode = Repo.get!(Episode, id)
+    episode = Episode |> Repo.get!(id) |> Repo.preload(:series)
     changeset = Episode.changeset(episode, episode_params)
 
     case Repo.update(changeset) do
