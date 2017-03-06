b63742329b34e9d4ad32f4e7538e29c9709c72da
Ethan Larkham <ethanlarkham@gmail.com>
2017-03-05 19:34:55 -0500

    Elm will now track window resizing

--- web/static/elm/Model.elm
+++ web/static/elm/Model.elm
@@ -6,21 +6,25 @@ import Pages.Login.Model as Login
 import Pages.Home.Model as Home
 import Models.User as User
 
+import Window
+
 type alias Model =
   { route : Route
   , token : String
   , logged_in : Bool
   , user : User.Model
+  , window: Window.Size
   , login_page: Login.Model
   , home_page: Home.Model
   }
 
-init_model : String -> Bool -> Model
-init_model token logged_in  =
+init_model : String -> Bool -> Window.Size -> Model
+init_model token logged_in window =
   { route = T.Home
   , token = token
   , logged_in = logged_in
   , user = User.init_model
+  , window = window
   , login_page = Login.init_model
   , home_page = Home.init_model
   }
--- web/static/js/index.js
+++ web/static/js/index.js
@@ -3,12 +3,20 @@
 // SCSS Loader
 require('../scss/main.scss')
 
-
 // Elm Loader
 let Elm = require('../elm/Main');
 
 let token = localStorage.getItem('token');
-let app = Elm.Main.fullscreen({token: token});
+let window = document.defaultView;
+
+let flags =
+  { token: token
+  , window: { height: window.innerHeight
+            , width: window.innerWidth
+            }
+  }
+
+let app = Elm.Main.fullscreen(flags);
 
 app.ports.store.subscribe(function(data){
   let [key, value] = data
