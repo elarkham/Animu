6010402ef3a0397020b5fb4789dfca549f8033f1
Ethan Larkham <ethanlarkham@gmail.com>
2017-03-13 23:37:34 -0400

    Created Video proccessing pipeline
    - Video path is given through Episode's video_path
    - Only WebM, MP4 and MKV are accepted
    - MKVs are remuxed into MP4s
    - MKV subtitles+fonts are extracted into sep folders
    - All output video metadata is saved and stored

---
+++ lib/animu/media/video/remux.ex
@@ -0,0 +1,125 @@
+defmodule Animu.Media.Video.Remux do
+  alias Animu.FFmpeg
+
+  @output_dir "videos"
+  @subtitles_dir "subtitles"
+  @font_dir "fonts"
+
+  def remux(video = %{input: %{format: "WebM"}}) do
+    with {:ok, video} <- keep_original(video),
+         {:ok, video} <- probe_output_file(video),
+         do: {:ok, video}
+  end
+
+  def remux(video = %{input: %{format: "MPEG-4"}}) do
+    with {:ok, video} <- keep_original(video),
+         {:ok, video} <- probe_output_file(video),
+         do: {:ok, video}
+  end
+
+  def remux(video = %{input: %{format: "Matroska"}, subtitles: nil}) do
+    with {:ok, video} <- generate_directories(video),
+         {:ok, video} <- convert_mkv_to_mp4(video),
+         {:ok, video} <- probe_output_file(video),
+         do: {:ok, video}
+  end
+
+  def remux(video = %{input: %{format: "Matroska"}}) do
+    with {:ok, video} <- generate_directories(video),
+         {:ok, video} <- convert_mkv_to_mp4(video),
+         {:ok, video} <- extract_subtitles(video),
+         {:ok, video} <- extract_fonts(video),
+         {:ok, video} <- probe_output_file(video),
+         do: {:ok, video}
+  end
+
+  def generate_directories(video = %{subtitles: nil}) do
+    video =
+      %{video | output: %{video.output | dir: @output_dir}}
+
+    case File.mkdir_p(video.output.dir) do
+      :ok ->
+        {:ok, video}
+      {:error, _} ->
+        {:error, "Failed To Create Output Directory"}
+    end
+  end
+
+  def generate_directories(video) do
+    video =
+      %{ video |
+         output: %{video.output | dir: @output_dir},
+         subtitles: %{video.subtitles | dir: @subtitles_dir},
+         font: %{video.font | dir: @font_dir},
+       }
+
+    with :ok <- File.mkdir_p(video.output.dir),
+         :ok <- File.mkdir_p(video.subtitles.dir),
+         :ok <- File.mkdir_p(video.font.dir) do
+      {:ok, video}
+    else
+      _ -> {:error, "Failed To Create Output Directories"}
+    end
+  end
+
+  def keep_original(video) do
+    {:ok, %{video | output: video.input}}
+  end
+
+  def probe_output_file(video) do
+    case FFmpeg.probe(video.output.path) do
+      {:error, reason} ->
+        {:error, reason}
+      {:ok, probe_data} ->
+        {:ok, %{video | output: %{video.output | probe_data: probe_data}}}
+    end
+  end
+
+  def convert_mkv_to_mp4(video = %{input: %{format: "Matroska"}}) do
+    extension = ".mp4"
+    filename = video.name <> extension
+    path = Path.join(video.output.dir, filename)
+    format = "MPEG-4"
+
+    output =
+      %{ video.output |
+         filename: filename,
+         extension: extension,
+         path: path,
+         format: format,
+       }
+
+    video  = %{video | output: output}
+
+    case FFmpeg.mkv_to_mp4(video.input.path, video.output.path) do
+      {_, 0} ->
+        {:ok, video}
+      {_, _} ->
+        {:error, "Failed To Convert MKV->MP4"}
+    end
+  end
+
+  def extract_subtitles(video = %{input: %{format: "Matroska"}}) do
+    filename = video.name <> ".ass"
+    path = Path.join(video.subtitles.dir, filename)
+
+    subtitles = %{video.subtitles | filename: filename}
+    video  = %{video | subtitles: subtitles}
+
+    case FFmpeg.extract_subtitles(video.input.path, path) do
+      {_, 0} ->
+        {:ok, video}
+      {_, _} ->
+        {:error, "Failed to Extract Subtitles"}
+    end
+  end
+
+  def extract_fonts(video = %{input: %{format: "Matroska"}}) do
+     case FFmpeg.extract_fonts(video.input.path, video.font.dir) do
+      {_, num} when num > 1 ->
+        {:error, "Failed To Extract Fonts"}
+      {_, _} ->
+        {:ok, video}
+     end
+  end
+end
---
+++ lib/animu/media/video/validate.ex
@@ -0,0 +1,173 @@
+defmodule Animu.Media.Video.Validate do
+  import Animu.FFmpeg, only: [probe: 1]
+
+  @valid_extensions ~w(.mkv .webm .mp4)
+
+  def validate_input(video) do
+    with {:ok, video} <- check_file_exists(video),
+         {:ok, video} <- check_valid_extension(video),
+         {:ok, video} <- probe_input_file(video),
+         {:ok, video} <- check_valid_format(video),
+         {:ok, video} <- isolate_video_track(video),
+         {:ok, video} <- isolate_audio_track(video),
+         {:ok, video} <- set_audio_track_language(video),
+         {:ok, video} <- check_for_subtitles(video),
+         {:ok, video} <- check_for_fonts(video),
+         do: {:ok, video}
+  end
+
+  def check_file_exists(video) do
+    case File.regular?(video.input.path) do
+      true ->
+        {:ok, video}
+      false ->
+        {:error, "Video Does Not Exist"}
+    end
+  end
+
+  def check_valid_extension(video) do
+    dir = Path.dirname(video.input.path)
+    filename = Path.basename(video.input.path)
+    extension = Path.extname(video.input.path)
+    name = Path.rootname(filename)
+
+    input =
+      %{ video.input |
+         dir: dir,
+         filename: filename,
+         extension: extension,
+       }
+
+    video =
+      %{ video |
+         input: input,
+         name: name,
+       }
+
+    case Enum.any?(@valid_extensions, &(&1 == extension)) do
+      true ->
+        {:ok, video}
+
+      false ->
+        {:error, "Video Has Unsupported Extension"}
+    end
+  end
+
+  def probe_input_file(video) do
+    case probe(video.input.path) do
+      {:ok, probe_data} ->
+        {:ok, %{video | input: %{video.input | probe_data: probe_data}}}
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  def check_valid_format(video = %{input: %{extension: ".mkv"}}) do
+    input = %{video.input | format: "Matroska"}
+    video = %{video | input: input}
+    case video.input.probe_data["format"]["format_name"] do
+      "matroska,webm" ->
+        {:ok, video}
+      _ ->
+        {:error, "Video Extension Does Not Match It's Format"}
+    end
+  end
+
+  def check_valid_format(video = %{input: %{extension: ".webm"}}) do
+    input = %{video.input | format: "WebM"}
+    video = %{video | input: input}
+    case video.input.probe_data["format"]["format_name"] do
+      "matroska,webm" ->
+        {:ok, video}
+      _ ->
+        {:error, "Video Extension Does Not Match It's Format"}
+    end
+  end
+
+  def check_valid_format(video = %{input: %{extension: ".mp4"}}) do
+    input = %{video.input | format: "MPEG-4"}
+    video = %{video | input: input}
+    case video.input.probe_data["format"]["format_name"] do
+      "mov,mp4,m4a,3gp,3g2,mj2" ->
+        {:ok, video}
+      _ ->
+        {:error, "Video Extension Does Not Match It's Format"}
+    end
+  end
+
+  def isolate_video_track(video) do
+    streams = video.input.probe_data["streams"]
+    search = Enum.filter(streams, &(&1["codec_type"] == "video"))
+    search_count = Enum.count(search)
+
+    cond do
+      search_count == 0 ->
+        {:error, "Video Does Not Contain A Video Track"}
+      search_count > 1 ->
+        {:error, "Video's With Multiple Video Track Are Not Supported"}
+      true ->
+        {:ok, %{video | video_track: List.first(search)}}
+    end
+  end
+
+  def isolate_audio_track(video) do
+    streams = video.input.probe_data["streams"]
+    search = Enum.filter(streams, &(&1["codec_type"] == "audio"))
+    search_count = Enum.count(search)
+
+    cond do
+      search_count == 0 ->
+        {:error, "Video Does Not Contain An Audio Track"}
+      search_count > 1 ->
+        {:error, "Video's With Multiple Audio Tracks Are Not Supported"}
+      true ->
+        {:ok, %{video | audio_track: List.first(search)}}
+    end
+  end
+
+  def set_audio_track_language(video) do
+    language =
+      try do
+        video.audio_track["tags"]["language"]
+      rescue
+        _ in KeyError -> nil
+      end
+
+    audio_track = Map.put(video.audio_track, "language", language)
+    video = %{video | audio_track: audio_track}
+
+    {:ok, video}
+  end
+
+  def check_for_subtitles(video) do
+    streams = video.input.probe_data["streams"]
+    search = Enum.filter(streams, &(&1["codec_name"] == "ass"))
+    search_count = Enum.count(search)
+
+    cond do
+      search_count == 0 ->
+        {:ok, %{video | subtitles: nil}}
+      search_count > 1 ->
+        {:error, "Video's With Multiple Subtitle Tracks Are Not Supported"}
+      true ->
+        {:ok, %{video | subtitles: %{video.subtitles | data: List.first(search)}}}
+    end
+  end
+
+  def check_for_fonts(video = %{subtitles: nil}), do: {:ok, video}
+
+  def check_for_fonts(video) do
+    streams = video.input.probe_data["streams"]
+    search = Enum.filter(streams,
+      &(&1["codec_name"] == "ttf" || &1["codec_name"] == "otf"))
+
+    search_count = Enum.count(search)
+    cond do
+      search_count == 0 ->
+        {:ok, %{video | font: nil}}
+      true ->
+        {:ok, %{video | font: %{video.font | data: search}}}
+    end
+  end
+
+end
