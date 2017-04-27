f5af39c0ed7984fb9229c5e85258ae499d7a99ed
Ethan Larkham <ethanlarkham@gmail.com>
2017-04-27 16:50:54 -0400

    Made it possible to disable augur based on ENV var

--- lib/animu/application.ex
+++ lib/animu/application.ex
@@ -1,26 +1,29 @@
 defmodule Animu.Application do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
+  def start(_type, args) do
     import Supervisor.Spec
 
-    # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
       supervisor(Animu.Repo, []),
       # Start the endpoint when the application starts
       supervisor(Animu.Web.Endpoint, []),
+    ]
 
+    augur = [
       # RSS feed reader
       worker(Augur.Reader, [Augur.Reader]),
       # Torrent Registry that tracks transmission downloads
       worker(Augur.TransmissionClient, [Augur.TransmissionClient]),
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
+    children =
+      case System.get_env("AUGUR") do
+        "false" -> children
+        _       -> children ++ augur
+      end
+
     opts = [strategy: :one_for_one, name: Animu.Supervisor]
     Supervisor.start_link(children, opts)
   end
