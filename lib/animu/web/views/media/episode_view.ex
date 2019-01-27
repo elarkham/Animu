cfd0fc22fdb3983a7ff0d9f6f197868e837030e9
Ethan Larkham <ethanlarkham@gmail.com>
2019-01-26 19:42:09 -0500

    Add more series info to episode view

--- lib/animu/web/views/media/episode_view.ex
+++ lib/animu/web/views/media/episode_view.ex
@@ -5,11 +5,32 @@ defmodule Animu.Web.EpisodeView do
   alias Animu.Web.SeriesView
 
   def render("index.json", %{episodes: episodes}) do
-    %{episodes: render_many(episodes, EpisodeView, "episodes.json")}
+    %{episodes: render_many(episodes, EpisodeView, "episode_index.json")}
   end
 
   def render("show.json", %{episode: episode}) do
     %{episode: render_one(episode, EpisodeView, "episode.json")}
+  end
+
+def render("episode_index.json", %{episode: episode}) do
+    %{id: episode.id,
+      title: episode.title,
+      synopsis: episode.synopsis,
+      thumbnail: episode.thumbnail,
+      kitsu_id: episode.kitsu_id,
+
+      number: episode.number,
+      season_number: episode.season_number,
+      airdate: episode.airdate,
+
+      series: %{
+        id: episode.series.id,
+        slug: episode.series.slug,
+        canon_title: episode.series.canon_title,
+      },
+
+      video: episode.video,
+    }
   end
 
   def render("episodes.json", %{episode: episode}) do
