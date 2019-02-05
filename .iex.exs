063859657c4202d97be61abaa888b2ca26ed4753
Ethan Larkham <ethanlarkham@gmail.com>
2019-02-05 11:26:06 -0500

    Remove Video alias from .iex

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,6 @@
 alias Animu.{Repo, Media, Account, Web}
 alias Animu.Media.{Series, Franchise, Episode, Video}
 
-alias Video.Bag
-require Bag
-import Bag
+#alias Video.Bag
+#require Bag
+#import Bag
